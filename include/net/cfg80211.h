849a2833b6ecdd661bd73d601c2ceb06db671c03
Johannes Berg <johannes.berg@intel.com>
2014-03-24 15:51:11 +0100

    cfg80211: allow userspace to take ownership of interfaces
    When dynamically creating interfaces from userspace, e.g. for P2P usage,
    such interfaces are usually owned by the process that created them, i.e.
    wpa_supplicant. Should wpa_supplicant crash, such interfaces will often
    cease operating properly and cause problems on restarting the process.
    
    To avoid this problem, introduce an ownership concept for interfaces. If
    an interface is owned by a netlink socket, then it will be destroyed if
    the netlink socket is closed for any reason, including if the process it
    belongs to crashed. This gives us a race-free way to get rid of any such
    interfaces.
    
    Change-Id: I6ee1ab3009e0deabc25a6e86bc49bdd39d651521
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/33658
    Tested-by: IWL Jenkins
    Reviewed-by: EliadX Peller <eliad@wizery.com>
    Reviewed-by: Ilan Peer <ilan.peer@intel.com>
    Tested-by: Ilan Peer <ilan.peer@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/33773
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    
    x-iwlwifi-stack-dev: af1774da8eb09211afce279a813d415e1b66ac45

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -3215,6 +3215,7 @@ struct cfg80211_cached_keys;
  * @ibss_dfs_possible: (private) IBSS may change to a DFS channel
  * @event_list: (private) list for internal event processing
  * @event_lock: (private) lock for event list
+ * @owner_nlportid: (private) owner socket port ID
  */
 struct wireless_dev {
 	struct wiphy *wiphy;
@@ -3260,6 +3261,8 @@ struct wireless_dev {
 
 	bool cac_started;
 	unsigned long cac_start_time;
+
+	u32 owner_nlportid;
 
 #ifdef CPTCFG_CFG80211_WEXT
 	/* wext data */
