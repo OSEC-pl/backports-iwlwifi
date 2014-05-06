e86bd577976f5a99d95fab8aceb11d262f25bdbf
Johannes Berg <johannes.berg@intel.com>
2014-05-06 08:30:42 +0200

    iwlwifi: mvm: fix uapsd_disable module parameter
    Later versions of the driver have uAPSD enabled unconditionally,
    so the backported patch introduced an error. Fix this.
    
    Change-Id: I00dcfe148f565285be80ffa65769ff3e0bd617f3
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/36597
    
    x-iwlwifi-stack-dev: bc784238fe6e06f16c95c30f3a9570dd9572aea3

--- drivers/net/wireless/iwlwifi/mvm/mac80211.c
+++ drivers/net/wireless/iwlwifi/mvm/mac80211.c
@@ -323,9 +323,6 @@ int iwl_mvm_mac_setup_register(struct iwl_mvm *mvm)
 		    IEEE80211_HW_SUPPORTS_DYNAMIC_SMPS |
 		    IEEE80211_HW_SUPPORTS_STATIC_SMPS;
 
-	if (!iwlwifi_mod_params.uapsd_disable)
-		hw->flags |= IEEE80211_HW_SUPPORTS_UAPSD;
-
 	hw->queues = mvm->first_agg_queue;
 	hw->offchannel_tx_hw_queue = IWL_MVM_OFFCHANNEL_QUEUE;
 	hw->radiotap_mcs_details |= IEEE80211_RADIOTAP_MCS_HAVE_FEC |
@@ -342,7 +339,8 @@ int iwl_mvm_mac_setup_register(struct iwl_mvm *mvm)
 	    !iwlwifi_mod_params.sw_crypto)
 		hw->flags |= IEEE80211_HW_MFP_CAPABLE;
 
-	if (mvm->fw->ucode_capa.flags & IWL_UCODE_TLV_FLAGS_UAPSD_SUPPORT) {
+	if (mvm->fw->ucode_capa.flags & IWL_UCODE_TLV_FLAGS_UAPSD_SUPPORT &&
+	    !iwlwifi_mod_params.uapsd_disable) {
 		hw->flags |= IEEE80211_HW_SUPPORTS_UAPSD;
 		hw->uapsd_queues = IWL_UAPSD_AC_INFO;
 		hw->uapsd_max_sp_len = IWL_UAPSD_MAX_SP;
