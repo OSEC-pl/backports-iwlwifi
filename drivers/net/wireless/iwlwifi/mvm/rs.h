f05deffacd95b7fd462bb2df04c49418ac767a0c
Eyal Shapira <eyal@wizery.com>
2015-05-27 22:20:46 +0300

    [BUGFIX] iwlwifi: mvm: rs: report last tx rate based on RSSI and caps
    In scenarios where we haven't converged yet to a specific modulation
    and rate it could be better to report to userspace the last tx rate
    based on the STA capabilities and RSSI. This is important as sometimes
    userspace displays the last tx rate as the link speed.
    This avoids being presented with low legacy rates when rs just begins
    its search or after an idle period in which it resets itself.
    
    type=bugfix
    bug=cq:MWG100234428
    fixes=unknown
    
    Change-Id: I78c428b2b40c852188cd6efea68d07b064b811bb
    Signed-off-by: Eyal Shapira <eyalx.shapira@intel.com>
    ---
    This better matches Ayelet's Link Indications SAS
    Marked as fixed=unknown as this wasn't something that was broken
    by a specific commit but was there all along.
    Reviewed-on: https://gerrit.rds.intel.com/r/57808
    Tested-by: IWL Jenkins
    Reviewed-by: Guy Cohen <guy.cohen@intel.com>
    x-iwlwifi-stack-dev: 7aceab485ae6c5c0ec40f2b03827d572cb0f7f29

--- drivers/net/wireless/iwlwifi/mvm/rs.h
+++ drivers/net/wireless/iwlwifi/mvm/rs.h
@@ -1,6 +1,7 @@
 /******************************************************************************
  *
  * Copyright(c) 2003 - 2014 Intel Corporation. All rights reserved.
+ * Copyright(c) 2015 Intel Mobile Communications GmbH
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of version 2 of the GNU General Public License as
@@ -345,6 +346,14 @@ struct iwl_lq_sta {
 	u8 max_siso_rate_idx;
 	u8 max_mimo2_rate_idx;
 
+	/* Optimal rate based on RSSI and STA caps.
+	 * Used only to reflect link speed to userspace.
+	 */
+	struct rs_rate optimal_rate;
+	unsigned long optimal_rate_mask;
+	const struct rs_init_rate_info *optimal_rates;
+	int optimal_nentries;
+
 	u8 missed_rate_counter;
 
 	struct iwl_lq_cmd lq;
@@ -374,6 +383,8 @@ struct iwl_lq_sta {
 #endif
 		u8 chains;
 		s8 chain_signal[IEEE80211_MAX_CHAINS];
+		s8 last_rssi;
+
 		struct iwl_mvm *drv;
 	} pers;
 };
