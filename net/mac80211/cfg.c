21cffc423f062d7c7482f8a360d54118693677e1
Arik Nemtsov <arik@wizery.com>
2015-07-12 16:54:58 +0300

    mac80211: debugfs: add file to disallow TDLS wider-bw
    Sometimes we are interested in testing TDLS performance in a specific
    width setting. Add the ability to disable the wider-band feature, thereby
    allowing the TDLS channel width to be controlled by the BSS width.
    
    type=feature
    
    Change-Id: I3a470d7c34a50685007732f556de252e06bb6ced
    Signed-off-by: Arik Nemtsov <arikx.nemtsov@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/r/59867
    Tested-by: IWL Jenkins
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/r/60147
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Tested-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    x-iwlwifi-stack-dev: 250208ee2cfd766e1329032b12deb885ccd629d6

--- net/mac80211/cfg.c
+++ net/mac80211/cfg.c
@@ -1098,6 +1098,7 @@ static int sta_apply_parameters(struct ieee80211_local *local,
 		set_sta_flag(sta, WLAN_STA_TDLS_CHAN_SWITCH);
 
 	if (test_sta_flag(sta, WLAN_STA_TDLS_PEER) &&
+	    !sdata->u.mgd.tdls_wider_bw_prohibited &&
 	    local->hw.flags & IEEE80211_HW_TDLS_WIDER_BW &&
 	    params->ext_capab_len >= 8 &&
 	    params->ext_capab[7] & WLAN_EXT_CAPA8_TDLS_WIDE_BW_ENABLED)
