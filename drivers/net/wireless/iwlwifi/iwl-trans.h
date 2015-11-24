3d47c3fbcbcc907316ce6fa7ebef90551db59244
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2015-11-24 13:24:24 +0200

    [BUGFIX] iwlwifi: pcie: allow the op_mode to block the tx queues
    In certain flows (see next patches), the op_mode may need to
    block the Tx queues for a short period. Provide an API for
    that. The transport is in charge of counting the number of
    times the queues are blocked since the op_mode may block the
    queues several times in a row before unblocking them.
    
    type=bugfix
    bug=not-tracked
    fixes=unknown
    
    Change-Id: I5f0513015f522821f3511357a27e4f71a0c74900
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/10104
    Reviewed-by: Berg, Johannes <johannes.berg@intel.com>
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/10794
    x-iwlwifi-stack-dev: b4078b37322b8b1ed019d7531c760788500b92b9

--- drivers/net/wireless/iwlwifi/iwl-trans.h
+++ drivers/net/wireless/iwlwifi/iwl-trans.h
@@ -533,6 +533,11 @@ struct iwl_trans_txq_scd_cfg {
  *	queue is set to awake. Must be atomic.
  * @dbgfs_register: add the dbgfs files under this directory. Files will be
  *	automatically deleted.
+ * @block_txq_ptrs: stop updating the write pointers of the Tx queues. Note
+ *	that the transport needs to refcount the calls since this function
+ *	will be called several times with block = true, and then the queues
+ *	need to be unblocked only after the same number of calls with
+ *	block = false.
  * @write8: write a u8 to a register at offset ofs from the BAR
  * @write32: write a u32 to a register at offset ofs from the BAR
  * @read32: read a u32 register at offset ofs from the BAR
@@ -597,6 +602,7 @@ struct iwl_trans_ops {
 	int (*wait_tx_queue_empty)(struct iwl_trans *trans, u32 txq_bm);
 	void (*freeze_txq_timer)(struct iwl_trans *trans, unsigned long txqs,
 				 bool freeze);
+	void (*block_txq_ptrs)(struct iwl_trans *trans, bool block);
 
 	void (*write8)(struct iwl_trans *trans, u32 ofs, u8 val);
 	void (*write32)(struct iwl_trans *trans, u32 ofs, u32 val);
@@ -1035,6 +1041,16 @@ static inline void iwl_trans_freeze_txq_timer(struct iwl_trans *trans,
 
 	if (trans->ops->freeze_txq_timer)
 		trans->ops->freeze_txq_timer(trans, txqs, freeze);
+}
+
+static inline void iwl_trans_block_txq_ptrs(struct iwl_trans *trans,
+					    bool block)
+{
+	if (unlikely(trans->state != IWL_TRANS_FW_ALIVE))
+		IWL_ERR(trans, "%s bad state = %d\n", __func__, trans->state);
+
+	if (trans->ops->block_txq_ptrs)
+		trans->ops->block_txq_ptrs(trans, block);
 }
 
 static inline int iwl_trans_wait_tx_queue_empty(struct iwl_trans *trans,
