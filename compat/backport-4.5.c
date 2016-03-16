dad33f7e0edcf92f53a51e7f4c1a1cee67eb07ed
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2016-03-16 11:24:36 +0200

    mac80211: disable uapsd based on OUIs coming from debugfs [NOUPSTREAM]
    The customer can add OUIs to a list that will be fed to
    mac80211 through debugfs. If the P2P GO / AP to which we
    associate has a vendor IE with an OUI in the list, we
    will disable uAPSD.
    
    type=feature
    
    Change-Id: I4e7923256d1d0c49fcc6f8d17df437873e49fac2
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/24004
    Reviewed-by: Coelho, Luciano <luciano.coelho@intel.com>
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/27687
    x-iwlwifi-stack-dev: 8c9602485005f3a0555692c00ba9344f2a75cd6e

--- compat/backport-4.5.c
+++ compat/backport-4.5.c
@@ -11,6 +11,8 @@
 #include <linux/leds.h>
 #include <linux/export.h>
 #include <linux/errno.h>
+#include <linux/slab.h>
+#include <asm/uaccess.h>
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3,19,0)
 int led_set_brightness_sync(struct led_classdev *led_cdev,
@@ -31,3 +33,34 @@ int led_set_brightness_sync(struct led_classdev *led_cdev,
 }
 EXPORT_SYMBOL_GPL(led_set_brightness_sync);
 #endif /* >= 3.19 */
+
+/**
+ * memdup_user_nul - duplicate memory region from user space and NUL-terminate
+ *
+ * @src: source address in user space
+ * @len: number of bytes to copy
+ *
+ * Returns an ERR_PTR() on failure.
+ */
+void *memdup_user_nul(const void __user *src, size_t len)
+{
+	char *p;
+
+	/*
+	 * Always use GFP_KERNEL, since copy_from_user() can sleep and
+	 * cause pagefault, which makes it pointless to use GFP_NOFS
+	 * or GFP_ATOMIC.
+	 */
+	p = kmalloc(len + 1, GFP_KERNEL);
+	if (!p)
+		return ERR_PTR(-ENOMEM);
+
+	if (copy_from_user(p, src, len)) {
+		kfree(p);
+		return ERR_PTR(-EFAULT);
+	}
+	p[len] = '\0';
+
+	return p;
+}
+EXPORT_SYMBOL_GPL(memdup_user_nul);
