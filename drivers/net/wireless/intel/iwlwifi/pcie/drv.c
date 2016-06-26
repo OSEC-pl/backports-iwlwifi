466d8300c189cab819405f3ec65e5957870e0cae
Oren Givon <oren.givon@intel.com>
2016-06-26 13:07:22 +0300

    iwlwifi: add new 8265 series PCI IDs
    Add 4 new 8265 series PCI IDs.
    
    type=feature
    
    Change-Id: Icd0c73e506dfa70738cd294abd174b8b2fdf818e
    Signed-off-by: Oren Givon <oren.givon@intel.com>
    x-iwlwifi-stack-dev: 67426b9eaf83b65ab612f265a0955e5cc64d66f5

--- drivers/net/wireless/intel/iwlwifi/pcie/drv.c
+++ drivers/net/wireless/intel/iwlwifi/pcie/drv.c
@@ -310,6 +310,10 @@ static const struct pci_device_id iwl_hw_card_ids[] = {
 	{IWL_PCI_DEVICE(0x24FD, 0x8050, iwl8265_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24FD, 0x8010, iwl8265_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24FD, 0x0810, iwl8265_2ac_cfg)},
+	{IWL_PCI_DEVICE(0x24FD, 0x0910, iwl8265_2ac_cfg)},
+	{IWL_PCI_DEVICE(0x24FD, 0x0930, iwl8265_2ac_cfg)},
+	{IWL_PCI_DEVICE(0x24FD, 0x0950, iwl8265_2ac_cfg)},
+	{IWL_PCI_DEVICE(0x24FD, 0x0850, iwl8265_2ac_cfg)},
 
 /* 9000 Series */
 	{IWL_PCI_DEVICE(0x9DF0, 0x2A10, iwl5165_2ac_cfg)},
