0d8ddb5eb21943869578c06d06b91076a3ec47dc
Liad Kaufman <liad.kaufman@intel.com>
2016-08-03 18:41:27 +0300

    [BUGFIX] iwlwifi: mvm: remove variable shadowing
    Variable "ac" defined twice. Fix that.
    
    type=bugfix
    bug=not-tracked
    fixes=Ifda05e27c025ccd4e125e863cdbab36cb6b2df5c
    
    Change-Id: I348c78bfbfcc52fe98215d7aa85beb32a886172b
    Signed-off-by: Liad Kaufman <liad.kaufman@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/49235
    Reviewed-by: Grumbach, Emmanuel <emmanuel.grumbach@intel.com>
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Coelho, Luciano <luciano.coelho@intel.com>
    (cherry picked from commit e41628624f063609fc7c702e23afdb3135ab4435)
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/50894
    x-iwlwifi-stack-dev: 992efcc91075dcdda8df7b0b84ce08b55ea7d99f

--- drivers/net/wireless/intel/iwlwifi/mvm/sta.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/sta.c
@@ -635,14 +635,14 @@ static int iwl_mvm_sta_alloc_queue(struct iwl_mvm *mvm,
 			.scd_queue = queue,
 			.enable = 0,
 		};
-		u8 ac;
+		u8 txq_curr_ac;
 
 		disable_agg_tids = iwl_mvm_remove_sta_queue_marking(mvm, queue);
 
 		spin_lock_bh(&mvm->queue_info_lock);
-		ac = mvm->queue_info[queue].mac80211_ac;
+		txq_curr_ac = mvm->queue_info[queue].mac80211_ac;
 		cmd.sta_id = mvm->queue_info[queue].ra_sta_id;
-		cmd.tx_fifo = iwl_mvm_ac_to_tx_fifo[ac];
+		cmd.tx_fifo = iwl_mvm_ac_to_tx_fifo[txq_curr_ac];
 		spin_unlock_bh(&mvm->queue_info_lock);
 
 		/* Disable the queue */
