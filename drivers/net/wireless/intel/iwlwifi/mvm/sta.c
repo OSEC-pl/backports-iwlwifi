247cd842c7a7b19157c7d7132711c699f355096e
Liad Kaufman <liad.kaufman@intel.com>
2017-03-21 17:13:16 +0200

    [BUGFIX] iwlwifi: mvm: support ibss in dqa mode
    Allow working IBSS also when working in DQA mode.
    This is done by setting it to treat the queues the
    same as a BSS AP treats the queues.
    
    type=bugfix
    bug=jira:WIFILNX-230
    fixes=I1ec9d713730ee2f55b72f87da3050b05d8d5d4d8
    
    Change-Id: I737f278707abf2fb683f95092ea0a4fbd52d972d
    Signed-off-by: Liad Kaufman <liad.kaufman@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    x-iwlwifi-stack-dev: 707abd38a3ba8305fd0d8c92e324bbf274c70e29

--- drivers/net/wireless/intel/iwlwifi/mvm/sta.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/sta.c
@@ -1906,7 +1906,8 @@ int iwl_mvm_send_add_bcast_sta(struct iwl_mvm *mvm, struct ieee80211_vif *vif)
 	lockdep_assert_held(&mvm->mutex);
 
 	if (iwl_mvm_is_dqa_supported(mvm) && !iwl_mvm_has_new_tx_api(mvm)) {
-		if (vif->type == NL80211_IFTYPE_AP)
+		if (vif->type == NL80211_IFTYPE_AP ||
+		    vif->type == NL80211_IFTYPE_ADHOC)
 			queue = mvm->probe_queue;
 		else if (vif->type == NL80211_IFTYPE_P2P_DEVICE)
 			queue = mvm->p2p_dev_queue;
@@ -2093,7 +2094,8 @@ int iwl_mvm_add_mcast_sta(struct iwl_mvm *mvm, struct ieee80211_vif *vif)
 	if (!iwl_mvm_is_dqa_supported(mvm))
 		return 0;
 
-	if (WARN_ON(vif->type != NL80211_IFTYPE_AP))
+	if (WARN_ON(vif->type != NL80211_IFTYPE_AP &&
+		    vif->type != NL80211_IFTYPE_ADHOC))
 		return -ENOTSUPP;
 
 	ret = iwl_mvm_add_int_sta_common(mvm, msta, maddr,
@@ -2115,6 +2117,14 @@ int iwl_mvm_add_mcast_sta(struct iwl_mvm *mvm, struct ieee80211_vif *vif)
 						    timeout);
 		vif->cab_queue = queue;
 	} else {
+		/*
+		 * In IBSS, ieee80211_check_queues() sets the cab_queue to be
+		 * invalid, so make sure we use the queue we want.
+		 * Note that this is done here as we want to avoid making DQA
+		 * changes in mac80211 layer.
+		 */
+		if (vif->type == NL80211_IFTYPE_ADHOC)
+			vif->cab_queue = IWL_MVM_DQA_GCAST_QUEUE;
 		iwl_mvm_enable_txq(mvm, vif->cab_queue, vif->cab_queue, 0,
 				   &cfg, timeout);
 	}
