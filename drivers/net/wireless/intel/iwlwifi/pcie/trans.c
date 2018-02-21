f617bc0ec27ad96ab04a629a0ca7a38f00802511
Luca Coelho <luciano.coelho@intel.com>
2018-02-21 14:20:30 +0200

    [BUGFIX] [NOUPSTREAM] iwlwifi: pcie: remove rescan code for < 3.14
    The pci functions we use for the actual rescan code do not exist in <
    3.14 kernels.  It's not trivial (if at all possible) to backport them,
    so simply skip the rescan in those ancient kernels.
    
    We were already doing that for ChromeOS, by patching the driver during
    iwl7000 generation.
    
    type=bugfix
    fixes=I9e2ac52ec9a9c776f9e3c2c4d6f99eda28ef8c71
    ticket=none
    restriction=noupstream
    
    Change-Id: I5d947c36a63827c77a93346542ac548d9fead747
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    x-iwlwifi-stack-dev: f4f3862d3b5f0d6ab9567bc80ecf2b8fe1fe3e88

--- drivers/net/wireless/intel/iwlwifi/pcie/trans.c
+++ drivers/net/wireless/intel/iwlwifi/pcie/trans.c
@@ -2066,6 +2066,10 @@ static void iwl_trans_pcie_rescan_wk(struct work_struct *wk)
 {
 	struct iwl_trans_pcie_rescan *rescan =
 		container_of(wk, struct iwl_trans_pcie_rescan, work);
+#if LINUX_VERSION_IS_LESS(3,14,0)
+	dev_err(&rescan->pdev->dev,
+		"Device disconnected - can't rescan on old kernels.\n");
+#else
 	struct pci_bus *parent;
 
 	pci_lock_rescan_remove();
@@ -2073,6 +2077,7 @@ static void iwl_trans_pcie_rescan_wk(struct work_struct *wk)
 	pci_stop_and_remove_bus_device(rescan->pdev);
 	pci_rescan_bus(parent);
 	pci_unlock_rescan_remove();
+#endif /* LINUX_VERSION_IS_LESS(3,14,0) */
 
 	pci_dev_put(rescan->pdev);
 
